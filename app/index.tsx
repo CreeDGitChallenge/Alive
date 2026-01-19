5819dbdd42fe4fd956e7b683b926c53f5f891a88
Matthieu <matthieu.clio@gmail.com>
2026-01-19 18:14:22 +0100

    fix(index): change import Biker
    This :
    import Biker from '../src/entities/Biker'
    
    was change by :
    import Biker from '../src/entities/biker'

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { View } from "react-native";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 // Entities
-import Biker from '../src/entities/Biker'
+import Biker from '../src/entities/biker'
 
 export default function Index() {
 
