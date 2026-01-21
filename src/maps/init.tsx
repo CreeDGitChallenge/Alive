3c9d881ead95f81bb372056889c5c59af392dd1a
CreeDXD <pclio92@gmail.com>
2026-01-21 22:21:44 +0100

    add map image to files and use it in app

--- src/maps/init.tsx
+++ src/maps/init.tsx
@@ -1,1 +1,42 @@
-// Init
+// React Native
+import { Dimensions, StyleSheet } from "react-native";
+// Reanimated
+import Animated from "react-native-reanimated";
+
+// Types
+type AnimatedStyle = {
+  transform: ({ translateX: number } | { translateY: number })[];
+};
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+
+// ⚠️ Use the REAL dimensions of your image
+const MAP_ASPECT_RATIO = 1024 / 1536; // example only
+
+export default function Map(props: { animatedStyle?: AnimatedStyle }) {
+  const { animatedStyle } = props;
+
+  return (
+    <Animated.Image
+      source={require("../../assets/game/maps/road_map.png")}
+      style={[styles.map, animatedStyle ? animatedStyle : undefined]}
+      resizeMode="contain"
+    />
+  );
+}
+
+// Map data
+export const MAP_DATA = {
+  width: SCREEN_WIDTH,
+  height: SCREEN_WIDTH / MAP_ASPECT_RATIO,
+};
+
+// Styles
+const styles = StyleSheet.create({
+  map: {
+    position: "absolute",
+    width: MAP_DATA.width,
+    height: MAP_DATA.height,
+    bottom: 0,
+  },
+});
