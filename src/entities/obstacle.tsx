3589f65afd47090b435c1e8b10df58ffbe3f2479
Matthieu <matthieu.clio@gmail.com>
2026-01-21 20:05:48 +0100

    refactor: biker and obstacle have been refactored

--- src/entities/obstacle.tsx
+++ src/entities/obstacle.tsx
@@ -1,5 +1,25 @@
 // React native
 import { StyleSheet } from "react-native";
+// Animated
+import Animated from "react-native-reanimated";
+
+export default function Obstacle () {
+    return (
+        <Animated.View
+            style={
+                [
+                    styleObstacle.obstacle,
+                    {
+                        top: 0,
+                        left: 0,
+                        width: OBSTACLE.width,
+                        height: OBSTACLE.height
+                    }
+                ]
+            }
+        />
+    );
+}
 
 // Obstacle data
 export const OBSTACLE = {
@@ -7,52 +27,12 @@ export const OBSTACLE = {
     y: 0,
     width: 150,
     height: 100
-}
+};
 
 // Style
-export const styleObstacle = StyleSheet.create({
+const styleObstacle = StyleSheet.create({
     obstacle: {
         position: 'absolute',
         backgroundColor: 'green',
     }
-})
-
-// React native
-// import { StyleSheet } from "react-native";
-// // Reanimed
-// import Animated from "react-native-reanimated";
-
-// const OBSTACLE = {
-//     x: 0,
-//     y: 0,
-//     width: 150,
-//     height: 100
-// };
-
-// export default function Obstacle () {
-//     return (
-//         <Animated.View 
-//             style={
-//                 [
-//                     style.obstacle,
-//                     {
-//                         left: 0,
-//                         top: 0,
-//                         width: OBSTACLE.width,
-//                         height: OBSTACLE.height
-//                     }
-//                 ]
-//             }
-//         />
-//     )
-// }
-
-// // Style
-// const style = StyleSheet.create({
-//     obstacle: {
-//         position: 'absolute',
-//         backgroundColor: 'green',
-//     }
-// })
-
-// export { OBSTACLE };
+});
--- src/scenes/stageOne.tsx
+++ src/scenes/stageOne.tsx
@@ -6,8 +6,8 @@ import { GestureDetector, Gesture } from 'react-native-gesture-handler';
 import Animated, { useSharedValue, useAnimatedStyle, clamp } from "react-native-reanimated";
 
 // Entities
-import { BIKER, styleBiker } from '@/src/entities/biker'
-import { OBSTACLE, styleObstacle } from "@/src/entities/obstacle"
+import Biker, { BIKERDATA } from '@/src/entities/biker'
+import Obstacle, { OBSTACLE } from "@/src/entities/obstacle"
 
 // Scripts
 import { isColliding } from "../scripts/isColliding";
@@ -17,8 +17,8 @@ export default function StageOne () {
     const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window")
 
     // Object position
-    const translateX = useSharedValue(SCREEN_WIDTH / 2 - BIKER.width / 2);
-    const translateY = useSharedValue(SCREEN_HEIGHT / 2 - BIKER.height / 2);
+    const translateX = useSharedValue(SCREEN_WIDTH / 2 - BIKERDATA.width / 2);
+    const translateY = useSharedValue(SCREEN_HEIGHT / 2 - BIKERDATA.height / 2);
     const offsetX = useSharedValue(translateX.value);
     const offsetY = useSharedValue(translateY.value);
     const bikerIsColliding = useSharedValue(false);
@@ -30,20 +30,20 @@ export default function StageOne () {
             const nextY = offsetY.value + event.translationY;
 
             // Define biker new position
-            BIKER.x = nextX
-            BIKER.y = nextY
+            BIKERDATA.x = nextX
+            BIKERDATA.y = nextY
 
             // Update position if the object is not colliding
-            if (!isColliding(BIKER, OBSTACLE) && !bikerIsColliding.value) {
+            if (!isColliding(BIKERDATA, OBSTACLE) && !bikerIsColliding.value) {
                 // Change the value on X only if not higher than the max screen width
-                translateX.value = clamp(nextX, 0, SCREEN_WIDTH - BIKER.width);
+                translateX.value = clamp(nextX, 0, SCREEN_WIDTH - BIKERDATA.width);
                 // Change the value on Y only if not higher than the max screen width
-                translateY.value = clamp(nextY, 0, SCREEN_HEIGHT - BIKER.height)
+                translateY.value = clamp(nextY, 0, SCREEN_HEIGHT - BIKERDATA.height)
 
             // Reset Biker position
             } else {
-                translateX.value = SCREEN_WIDTH / 2 - BIKER.width / 2
-                translateY.value = SCREEN_HEIGHT / 2 - BIKER.height / 2
+                translateX.value = SCREEN_WIDTH / 2 - BIKERDATA.width / 2
+                translateY.value = SCREEN_HEIGHT / 2 - BIKERDATA.height / 2
                 bikerIsColliding.value = true
             }
         })
@@ -65,33 +65,13 @@ export default function StageOne () {
         <View>
             <GestureDetector gesture={panGesture}>
                 {/* Biker */}
-                <Animated.Image
-                    source={require('@/assets/game/biker/Biker.png')}
-                    style={
-                        [
-                            styleBiker.biker,
-                            animatedStyle
-                        ]
-                    }
-                    resizeMode={'contain'}
+                <Biker 
+                    animatedStyle={animatedStyle}
                 />
             </GestureDetector>
-            
 
             {/* Obstacle */}
-            <Animated.View 
-                style={
-                    [
-                        styleObstacle.obstacle,
-                        {
-                            top: 0,
-                            left: 0,
-                            width: OBSTACLE.width,
-                            height: OBSTACLE.height
-                        }
-                    ]
-                }
-            />
+            <Obstacle />
         </View>
     );
 }
