560080f57be9596c89ea1179a3c85f3358331cfa
Matthieu <matthieu.clio@gmail.com>
2026-01-30 22:43:19 +0100

    fix: change game loop

--- src/entities/obstacle/obstacleActions/movResetObstacle.ts
+++ src/entities/obstacle/obstacleActions/movResetObstacle.ts
@@ -1,28 +1,34 @@
 // Reanimated
 import { SharedValue } from "react-native-reanimated";
 
+type ObstacleType = {
+    x: SharedValue<number>;
+    y: SharedValue<number>;
+    width: SharedValue<number>;
+    height: SharedValue<number>;
+}
 type obstaclePositionType = {
     x: number;
     y: number;
-}[]
+}[];
 // - OBSTACLE: RESET / MOVEMENT -
 export const movementAndReset = (
-    translateObstacleY:SharedValue<number>, 
-    translateObstacleX: SharedValue<number>,
-    SCREEN_HEIGHT: number,
-    OBSTACLEPOSITION: obstaclePositionType,
+    obstacle: ObstacleType,
+    screenHeight: number,
+    obstaclePosition: obstaclePositionType,
     SPEED: number
 ) => {
+    'worklet';
     // Obstacle one reset
-    if (translateObstacleY.value >= SCREEN_HEIGHT) {
+    if (obstacle.y.value >= screenHeight) {
         // Define random number
         // const randNumb = randomNumber(3); 
-        const randNumb = Math.floor(Math.random() * 3)// Min 0, Max 2
+        const randNumb = Math.floor(Math.random() * obstaclePosition.length)// Min 0, Max 2
         // Reset
-        translateObstacleX.value = OBSTACLEPOSITION[randNumb].x;
-        translateObstacleY.value = 0;
+        obstacle.x.value = obstaclePosition[randNumb].x;
+        obstacle.y.value = -obstacle.height.value;
     // Obstacle one movements
     } else {
-        translateObstacleY.value = translateObstacleY.value + SPEED;
+        obstacle.y.value += SPEED;
     }
 }
